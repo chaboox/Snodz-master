06da4e1c8a9f18db137fb67f05a01f42740cb3d2
chaboox <chaboox@gmail.com>
2018-06-03 14:14:10 +0000

    add a comment

--- app/src/main/java/choor/boos/snodz/ajoutArticle.java
+++ app/src/main/java/choor/boos/snodz/ajoutArticle.java
@@ -48,7 +48,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class ajoutArticle extends AppCompatActivity {
-    String id;
+    String id;//yoo
     int key;
     long idArticle;
     EditText nom, prixAchat,prixVente,taxe,quantite,type;
